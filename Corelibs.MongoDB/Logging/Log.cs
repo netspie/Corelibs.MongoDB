c31c54348a8ca8f98a38f92ca750d00c1f299c78
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-02 13:57:53 +0100

    Add time create to log

--- Corelibs.MongoDB/Logging/Log.cs
+++ Corelibs.MongoDB/Logging/Log.cs
@@ -6,23 +6,27 @@ namespace Corelibs.MongoDB.Logging
 {
     public class Log : Entity
     {
+        public DateTime TimeCreated { get; private set; }
         public List<string> Messages { get; private set; } = new();
 
         public Log(string message) 
         {
             ID = Guid.NewGuid().ToString();
+            TimeCreated = DateTime.UtcNow;
             Messages.Add(message);
         }
 
         public Log(object @object)
         {
             ID = Guid.NewGuid().ToString();
+            TimeCreated = DateTime.UtcNow;
             Messages.Add(@object.ToString());
         }
 
         public Log(object @object, string message)
         {
             ID = Guid.NewGuid().ToString();
+            TimeCreated = DateTime.UtcNow;
             Messages.Add(@object.ToString());
             Messages.Add(message);
         }
