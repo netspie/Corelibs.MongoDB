ab54f80d3020d81991b13244eee00f48f2326b28
drlsn <dariusz.lusnia@gmail.com>
2023-09-25 01:40:26 +0200

    Make event store be able to serilize field properly

--- Corelibs.MongoDB/MongoDbEventStore.cs
+++ Corelibs.MongoDB/MongoDbEventStore.cs
@@ -2,6 +2,7 @@
 using Corelibs.Basic.DDD;
 using Corelibs.Basic.UseCases;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -13,12 +14,14 @@ namespace Corelibs.MongoDB;
 public class MongoDbEventStore : IEventStore
 {
     private readonly MongoConnection _connection;
+    private readonly Dictionary<string, Type> _eventTypes;
     private readonly string _collectionName;
 
     public MongoDbEventStore(
-        MongoConnection connection)
+        MongoConnection connection, Dictionary<string, Type> eventTypes)
     {
         _connection = connection;
+        _eventTypes = eventTypes;
         _collectionName = "events";
     }
 
@@ -56,16 +59,32 @@ public class MongoDbEventStore : IEventStore
         return Result.Success();
     }
 
-    async Task<Result<BaseDomainEvent[]>> IEventStore.Dequeue(int count = 1)
+    async Task<Result<BaseDomainEvent[]>> IEventStore.Peek(int count = 1)
     {
-        var collection = _connection.Database.GetCollection<BaseDomainEvent>(_collectionName);
+        var collection = _connection.Database.GetCollection<BsonDocument>(_collectionName);
 
         try
         {
             var hint = new BsonDocument(nameof(BaseDomainEvent.Timestamp), 1);
 
-            var filter = Builders<BaseDomainEvent>.Filter.Empty;
-            var items = await collection.Find(filter, new FindOptions { Hint = hint }).Limit(count).ToListAsync();
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            var docs = await collection.Find(filter, new FindOptions { Hint = hint }).Limit(count).ToListAsync();
+
+            var items = docs.Select(document =>
+            {
+                var eventName = document["_t"].AsString;
+                if (_eventTypes.ContainsKey(eventName))
+                {
+                    var targetType = _eventTypes[eventName];
+                    var serializer = BsonSerializer.LookupSerializer(targetType);
+                   
+                    return (BaseDomainEvent) BsonSerializer.Deserialize(document, targetType);
+                }
+                else
+                {
+                    throw new Exception("Unknown discriminator value.");
+                }
+            }).ToArray();
 
             return Result<BaseDomainEvent[]>.Success(items.ToArray());
         }
@@ -74,5 +93,24 @@ public class MongoDbEventStore : IEventStore
             Console.WriteLine(ex.ToString());
             return Result<BaseDomainEvent[]>.Failure(ex);
         }
+    }
+
+    async Task<Result> IEventStore.Delete(BaseDomainEvent ev)
+    {
+        var collection = _connection.Database.GetCollection<BaseDomainEvent>(_collectionName);
+
+        try
+        {
+            var filter = Builders<BaseDomainEvent>.Filter.Eq("_id", ev.Id);
+            var result = await collection.DeleteOneAsync(filter);
+            if (result.DeletedCount > 0)
+                return Result.Success();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        return Result.Failure();
     }
 }
